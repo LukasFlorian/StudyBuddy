d5d0aeaf900b0e8aaa3c4b364a797feb23707309
LukasFlorian <Lukas.richter.1@gmx.net>
2025-02-20 08:01:19 +0100

    Adapted login form to support HTTP post

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const path = require("path")
+const bodyParser = require("body-parser")
 
 router = express.Router()
 
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+router.use(bodyParser.json());
+
 router.get('/', (req, res) =>{
+    console.log("User opening homepage");
+    console.log(req.body);
+    res.sendFile(path.join(__dirname, "../../static/homepage.html"));
+})
+router.get('/homepage', (req, res) =>{
     console.log("User opening homepage");
     res.sendFile(path.join(__dirname, "../../static/homepage.html"));
 })
