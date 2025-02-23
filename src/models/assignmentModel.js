1074ef4c0de4a79ae57cee191aeabb608f6b47ec
LukasFlorian <Lukas.richter.1@gmx.net>
2025-02-23 17:42:28 +0100

    Added comments in src

--- src/models/assignmentModel.js
+++ src/models/assignmentModel.js
@@ -1,8 +1,11 @@
+// mongoose import
 const mongoose = require("mongoose");
 
+// schema definition
 const assignmentSchema  = new mongoose.Schema({
   tagID: { type: mongoose.Types.ObjectId, required: true },
   docID: { type: mongoose.Types.ObjectId, required: true },
 });
 
+// module export
 module.exports = mongoose.model("Assignment", assignmentSchema);
--- src/routes/uploadRoute.js
+++ src/routes/uploadRoute.js
@@ -1,43 +1,48 @@
+// Module imports
 const express = require("express");
-
-const router = express.Router();
 const Doc = require("../models/docModel");
 const User = require("../models/userModel");
+
+// router instantiation
+const router = express.Router();
 
 
 
 /*
-POST-Request für Upload:
-•⁠  ⁠User schickt bei Button-Klick POST-Request an api/documents/upload mit documentTitle, userID und den Tags im Body
-•⁠  ⁠⁠Ich schicke dir dann eine Meldung mit status
-		200, wenn es geklappt hat
-		400, wenn ich die userID nicht finden sollte (dürfte zwar nicht passieren, aber wäre der einzige nicht-serverseitige Fehler, der mir einfällt)
-		500 sonst
-
-auf Route /api/upload
+Upload POST-Request logic:
+- Client sends post request to api/upload with documentTitle, userID and the tags in the Body
+- Server responds:
+	200 in case of a successful upload
+	400 in case of an invalid userID
+	500 otherwise
 */
 router.post("/", async (req, res) => { 
     try {
         const body = req.body;
-
-        const fileObj = req.files.uploadFile; // Wir verwenden express-fileupload, daher req.files.uploadFile statt req.files.file
+        
+        const fileObj = req.files.uploadFile; // express-fileupload for file handling
+        // Making sure file was sent
         if (!fileObj) {
             return res.status(400).json({ message: "File is missing" });
         }
+        // Destructuring into file contents and metadata
         const fileBuffer = fileObj.data;
-        const fileType = fileObj.mimetype; // neu
-        const originalName = fileObj.name; // neu
+        const fileType = fileObj.mimetype;
+        const originalName = fileObj.name;
         
+        // Destructuring request body
         const title = body.docTitle;
         const description = body.description;
         const tag = body.tag;
         const userID = body.userID;
         
+        // Checking if user exists
         const user = await User.findById(userID).exec();
         if (!user) {
             return res.status(400).json({ message: "Your User ID does not exist" });
         }
 
+        // Creating new document
         const uploadDate = new Date();
         const doc = new Doc({
             userID,
@@ -45,14 +50,16 @@ router.post("/", async (req, res) => {
             uploadDate,
             description,
             file: fileBuffer,
-            fileType,       // neu
-            originalName,   // neu
+            fileType,
+            originalName,
             tag,
         });
 
+        // Saving document to database
         await doc.save();
         return res.status(200).json({ message: "Doc saved successfully" });
     } catch (err) {
+        // Error handling
         console.log(err);
         res.status(500).json({ message: "Server Error" });
     }
