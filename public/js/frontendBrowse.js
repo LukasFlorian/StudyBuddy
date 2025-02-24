d97d5cf3dd4735ca5fa700139e5e205342174783
nils11111 <nils.together@gmail.com>
2025-02-24 09:35:51 +0100

    experiment browsecards

--- public/js/frontendBrowse.js
+++ public/js/frontendBrowse.js
@@ -28,15 +28,17 @@ document.addEventListener("DOMContentLoaded", () => {
         // Für jedes Dokument wird eine Browse Card erstellt
         data.documents.forEach(doc => {
           const card = document.createElement("div");
-          card.classList.add("browse-card");
+          card.classList.add("browse-grid-container");
 
           // Hier kannst du das Layout der Card anpassen – wenn mehr Dokumente vorliegen, ggf. unterschiedliche Styles/pagination
           card.innerHTML = `
-            <h2>${doc.docTitle}</h2>
+            <div class="browse-card">
+            <img src="../public/img/browse_placeholder.png" alt="Search result" />
+            <p>${doc.docTitle}</p>
             <p>${doc.docDescription}</p>
             <p>Author: ${doc.docAuthor}</p>
-            <p>Date: ${new Date(doc.docDate).toLocaleString()}</p>
             <button class="download-btn" data-id="${doc.docID}">Download</button>
+            </div>
           `;
           resultsSection.appendChild(card);
         });
