23e9528b4310a3b0cb80cd2f283a431d4440b320
LukasFlorian <Lukas.richter.1@gmx.net>
2025-02-24 21:22:23 +0100

    Doc commit

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -1,16 +1,9 @@
 // module imports
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
 
 // router instantiation
 const router = express.Router();
-
-// middleware
-router.use(bodyParser.urlencoded({
-    extended: true
-}));
-router.use(bodyParser.json());
 
 // homepage route
 router.get('/', (req, res) =>{
